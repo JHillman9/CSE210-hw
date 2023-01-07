03f09fea64879fe5fb20bc0560530ae8901bfc78
Justin Hillman <jhillman9@byui.edu>
2023-01-07 16:39:05 -0700

    Made a simpe system to determine the user's letter grade and if they passed the class or not

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string sGrade = Console.ReadLine();
+        int grade = int.Parse(sGrade);
+        string lGrade = "F";
+
+        if (grade >= 90)
+        {
+            lGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            lGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            lGrade = "C";
+        }
+        else if(grade >= 60)
+        {
+            lGrade = "D";
+        }
+    Console.WriteLine($"You have an {lGrade} in the class");
+
+        if (grade > 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You failed the class");
+        }
+
+
     }
 }
